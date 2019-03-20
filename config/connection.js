6bff3f32f7284e728dc5f94843fcd01624c1d69c
Kenny <nels5410@gmail.com>
2019-03-19 21:28:57 -0500

    <message>

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
-require("dotenv").config();
+// require("dotenv").config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_Port,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-});
+});*/
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "3dvVfQ6Wgqm3fK9s",
+    database: "burgers_db"
+  });
 
 connection.connect(function(err) {
     if (err) {
