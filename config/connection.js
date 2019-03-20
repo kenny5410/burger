ae05d5d2eac73ca83b533d7c3ad85034c8ace371
Kenny <nels5410@gmail.com>
2019-03-20 16:44:07 -0500

    <message>

--- config/connection.js
+++ config/connection.js
@@ -20,7 +20,7 @@ var connection;
 // });
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWS_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
