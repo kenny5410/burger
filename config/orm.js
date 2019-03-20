2a945d799ac5b89bbed5d209867aeb719e3ffd4e
Kenny <nels5410@gmail.com>
2019-03-20 16:52:27 -0500

    <message>

--- config/orm.js
+++ config/orm.js
@@ -30,7 +30,7 @@ var orm = {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
-                throw err;
+                console.log(err);
             }
             cb(result);
         });
@@ -49,7 +49,7 @@ var orm = {
 
         connection.query(queryString, vals, function(err, result) {
             if (err) {
-                throw err;
+                console.log(err);
             }
             cb(result);
         });
@@ -65,7 +65,7 @@ var orm = {
         console.log(queryString);
         connection.query(queryString, function(err, result) {
             if (err) {
-                throw err;
+                console.log(err);
             }
             cb(result);
         });
